7ea07f6eec9b908a09a49168acd8f62f5671cfb7
eMobilis <sandramukwambo@gmail.com>
2023-04-20 15:03:24 +0300

    first commit

---
+++ src/main/kotlin/Datatypes.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    //string
+    var city:String = "Nairobi"
+    //Char------you use single quote
+    var domain:Char = 'k'
+    //Int
+    var age:Int = 20
+    //Boolean
+    var status:Boolean = true
+    //Float
+    var temperature:Float = 35.9F
+    //Double
+    var result:Double = 23.45632
+
+}
